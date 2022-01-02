34c3d24fa7cacb333ecec4295ff1df44c9cae03b
WilliamKennyAK <william.j.kenny.ak@gmail.com>
2022-01-01 16:35:44 -0900

    add comments in config_example.py

--- code/config_example.py
+++ code/config_example.py
@@ -1,9 +1,17 @@
-ssid="Your SSID"
-ssidPass = "Your SSID Password"
-interval=3
-title="office"
+# WiFi SSID
+ssid="Your WiFi SSID"
+# WiFi Password
+ssidPass = "Your WiFi Password"
+# Sensor Refresh Interval (seconds)
+interval=60
+# MQTT and screen title
+title="bedroom"
+# DHTxx GPIO Pin
 dht=14
+# Button GPIO Pin
 btn=12
+# oled Display GPIO Pins
 sda=4
 scl=5
-timeout=30
+#Screen Timeout (seconds)
+timeout=240
